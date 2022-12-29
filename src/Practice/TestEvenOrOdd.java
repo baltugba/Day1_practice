76173888224ba9fcf229400fc13247c7ad6e567f
tt_ba <baltugba.usa@gmail.com>
2022-12-29 13:23:41 -0500

    I write a method for evenOrOdd

---
+++ src/Practice/TestEvenOrOdd.java
@@ -0,0 +1,7 @@
+package Practice;
+
+public class TestEvenOrOdd {
+    public static void main(String[] args) {
+        evenOrOdd1.evenOrOdd(5);
+    }
+}
---
+++ src/Practice/evenOrOdd1.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class evenOrOdd1 {
+
+    public static void evenOrOdd(int number){
+        if(number %2 ==0){
+            System.out.println(number +" is even");
+        }else{
+            System.out.println(number +" is odd");
+        }
+
+    }
+}
